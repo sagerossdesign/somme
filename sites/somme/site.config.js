077a0cd33381915f38ec4e03f122cfa7c4601fcd
sagerossdesign <sagerossdesign@gmail.com>
2026-04-07 17:40:19 -0700

    Update homepage background to Add Noise 1080

--- sites/somme/site.config.js
+++ sites/somme/site.config.js
@@ -23,7 +23,7 @@ export const siteConfig = {
   },
   sections: {
     background: {
-      imageSrc: '/web%20assets/Add%20Noise.png',
+      imageSrc: '/web%20assets/Add%20Noise%201080.png',
       imageAlt: 'sōmme homepage background',
     },
     header: {
